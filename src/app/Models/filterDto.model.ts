d9cdb2405726d4009c37b952a065c93f597f2b49
Harun Yiğit <f211220029@ktun.edu.tr>
2025-04-08 19:47:15 +0300

    multi option select feauture

---
+++ src/app/Models/filterDto.model.ts
@@ -0,0 +1,4 @@
+export interface FilterDto{
+    optionIds:number[];
+    questionId:number;
+}
--- src/app/question-analysis/question-analysis.component.ts
+++ src/app/question-analysis/question-analysis.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Research } from '../Models/research.model';
 import { DataService } from '../data.service';
 import { FormsModule } from '@angular/forms';
+import { FilterDto } from '../Models/filterDto.model';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-question-analysis',
@@ -17,9 +19,16 @@ export class QuestionAnalysisComponent {
   errorMessage: string | null = null;
   id!: number;
   selectedOption: any = null;
+  selectedOptions: number[] = []; 
   selectedQuestion: any = null;
   analysisResult: any = null;
   errorMessageCheck: string | null = null;
+  selectionsCompleted: boolean = false;
+  filterDto: FilterDto = {
+    optionIds: [],
+    questionId: 0
+  };
+
 
   constructor( private dataService:DataService){}
 
@@ -27,6 +36,8 @@ export class QuestionAnalysisComponent {
     this.id=Number(localStorage.getItem("ResearchId"));
     this.getResearchesById(this.id);
   }
+
+  
 
   getResearchesById(id:number): void {
 
@@ -54,14 +65,21 @@ export class QuestionAnalysisComponent {
 
   // Hesapla
   calculate(): void {
-    if (this.selectedOption && this.selectedQuestion) {
+    if (this.filterDto) {
       console.log('API request gönderiliyor:', {
         optionId: this.selectedOption,
         questionId: this.selectedQuestion
       });
+
+     this.filterDto = {
+      optionIds:this.selectedOptions,
+      questionId: this.selectedQuestion
+     }
+      console.log("filterDto:",this.filterDto);
+
   
       // API request yap
-      this.dataService.getQuestionParticipantPercentage(this.selectedOption, this.selectedQuestion).subscribe(response => {
+      this.dataService.getQuestionParticipantPercentage(this.filterDto).subscribe(response => {
         this.analysisResult = response.map((item: any) => {
           // Seçeneklerin listesinden optionId'ye karşılık gelen optionText'i bul
           const question = this.research.questions.find(q => q.id === this.selectedQuestion);
@@ -80,6 +98,30 @@ export class QuestionAnalysisComponent {
       });
     }
   }
+
+  toggleOption(optionId: number): void {
+    const index = this.selectedOptions.indexOf(optionId);
+    if (index > -1) {
+      this.selectedOptions.splice(index, 1); // zaten seçiliyse kaldır
+    } else {
+      this.selectedOptions.push(optionId); // seçili değilse ekle
+    }
+    console.log("Selected Options:", this.selectedOptions);
+  }
+
+  completeSelection(): void {
+    if (this.selectedOptions.length === 0) {
+      alert("Lütfen en az bir seçenek seçin.");
+      return;
+    }
+    this.selectionsCompleted = true;
+    this.filterDto.optionIds = this.selectedOptions;
+    console.log("Seçilen Seçenekler:", this.selectedOptions);
+  }
+
+  
+
+  
 
 
 
